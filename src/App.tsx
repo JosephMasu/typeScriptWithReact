ba8dd40d4aa84c51069f0d5b8e96ffba96a41b47
Joe Masu <josephmuhindo089@gmail.com>
2024-07-07 13:59:51 +0300

    events

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 // import Component from "./starter/02-props";
-import Component from "./starter/03-state";
+// import Component from "./starter/03-state";
+import Component from "./starter/04-events";
+
 function App() {
   return (
     <main>
--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -1,9 +1,33 @@
+import React, { useState } from "react";
 function Component() {
+  const [text, setText] = useState('');
+  const [email, setemail] = useState('');
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+   console.log(e.target.value);
+   setemail(e.target.value);
+    
+  }
   return (
-    <div>
+    <section>
       <h2>React & Typescript</h2>
-      <h2>Events</h2>
-    </div>
+      <form className="form">
+        <input 
+          type="text" 
+          className="form-input mb-1" 
+          value={text} 
+          onChange={(e)=>e.target.value}
+        />
+        <input 
+          type="email" 
+          className="form-input mb-1" 
+          value={email} 
+          onChange={handleChange}
+        />
+      </form>
+      <button type="submit" className="btn btn-block">
+        Submit
+      </button>
+    </section>
   );
 }
 export default Component;
