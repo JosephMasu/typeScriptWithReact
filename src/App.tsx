928db739795e15fa79eb0a85b69648b8710d1a2d
Joe Masu <josephmuhindo089@gmail.com>
2024-07-07 13:30:38 +0300

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import Component from "./starter/02-props";
+// import Component from "./starter/02-props";
+import Component from "./starter/03-state";
 function App() {
   return (
     <main>
-      //02-props
-      <Component name='peter'id={123}>
+      {/* //02-props */}
+      {/* <Component name='peter'id={123}>
         <h2>Hello </h2>
       </Component>
-      <Component name='peter'id={123}></Component>
+      <Component name='peter'id={123}></Component> */}
+      <Component></Component>
     </main>
   );
 }
--- src/starter/03-state/index.tsx
+++ src/starter/03-state/index.tsx
@@ -1,8 +1,51 @@
+import { useState } from "react";
+type Link={
+  id: number;
+  url: string;
+  text: string;
+}
+const navLinks=[
+  {
+  id:1,
+  url:'some url',
+  text:'some text',
+},
+{
+  id:2,
+  url:'some url2',
+  text:'some text2',
+},
+{
+  id:3,
+  url:'some url3',
+  text:'some text3',
+}
+]
 function Component() {
+  const [text, setText] = useState('ShakeAnd Bake');
+  const [number, setnumber] = useState(23);
+  const [list, setLIST] = useState<string[]>([]);
+  const [links, setLinks]= useState<Link[]>(navLinks);
+  const [showText, setShowText] = useState(false);
+  const [showList, setShowList] = useState(false);
+  const [showNumber, setShowNumber] = useState(false);
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>State</h2>
+      <h2 className="mb-1">React & Typescript</h2>
+      {showText && <p>{text}</p>}
+      {showNumber && <p>{number}</p>}
+      {showList && <p>{list}</p>}
+      <button className="btn btn-center" onClick={()=>
+        {
+          setText('Hi Mom');
+          setnumber(23);
+          setLIST(['padre'+ " " +'madre']);
+          setLinks([...links, {id:4, url:'hello', text:'hello there'}])
+          setShowList(true)
+          setShowText(true);
+          setShowNumber(true);
+        }
+      }>click Me</button>
     </div>
   );
 }
