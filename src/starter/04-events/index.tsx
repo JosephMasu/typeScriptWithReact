8ca1c973313374ed4a1f6c8d8f921b87b724f7ee
Joe Masu <josephmuhindo089@gmail.com>
2024-07-07 14:44:46 +0300

    events last part

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -1,33 +1,56 @@
 import React, { useState } from "react";
+type Person = {
+  name: string;
+};
 function Component() {
   const [text, setText] = useState('');
-  const [email, setemail] = useState('');
-  const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
-   console.log(e.target.value);
-   setemail(e.target.value);
+  const [email, setEmail] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value);
+    setEmail(e.target.value);
+  }
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value);
+    setText(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+    const text = formData.get('text') as string;
+    const Person: Person = {name:data.text as string}
+    console.log(text, Person);
     
   }
+
   return (
     <section>
       <h2>React & Typescript</h2>
-      <form className="form">
-        <input 
-          type="text" 
-          className="form-input mb-1" 
-          value={text} 
-          onChange={(e)=>e.target.value}
+      <form onSubmit={handleSubmit} className="form">
+        <input
+          type="text"
+          className="form-input mb-1"
+          value={text}
+          name="text"
+          onChange={handleTextChange}
         />
-        <input 
-          type="email" 
-          className="form-input mb-1" 
-          value={email} 
+        <input
+          type="email"
+          className="form-input mb-1"
+          value={email}
+          name="email"
           onChange={handleChange}
         />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
       </form>
-      <button type="submit" className="btn btn-block">
-        Submit
-      </button>
     </section>
   );
 }
+
 export default Component;
