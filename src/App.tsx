d9293f0448f244c157c1d1d205db6c6a3506f3bd
Joe Masu <josephmuhindo089@gmail.com>
2024-07-08 16:12:06 +0300

    Redux Toolkit rtk

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
 // import Component from "./starter/04-events";
 // import Component from "./starter/06-context";
 // import Component from "./starter/07-reducers";
-import Component from "./starter/08-fetch-data";
+import Component from "./starter/09-rtk";
 
 
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
+import { store } from './store';
+import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+//   <QueryClientProvider client={queryClient}>
+//     <App />
+//   </QueryClientProvider>
+<Provider store={store}>
+<App />
+</Provider>
 );
