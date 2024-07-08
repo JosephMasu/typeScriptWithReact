7861749b9021d0fbfc28f7e5292d019124be44d9
Joe Masu <josephmuhindo089@gmail.com>
2024-07-08 14:53:02 +0300

    fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@
 // import Component from "./starter/03-state";
 // import Component from "./starter/04-events";
 // import Component from "./starter/06-context";
-import Component from "./starter/07-reducers";
+// import Component from "./starter/07-reducers";
+import Component from "./starter/08-fetch-data";
+
 
 
 
---
+++ src/starter/08-fetch-data/types.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod';
+import axios from 'axios';
+
+const url = 'https://www.course-api.com/react-tours-project';
+
+export const tourSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    image: z.string(),
+    info: z.string(),
+    price: z.string(),
+    // someValue: z.string(),
+  });
+  
+  export type Tour = z.infer<typeof tourSchema>;
+  //using axios
+  export const fetchTours = async():Promise<Tour[]>=>{
+    const response = await axios.get<Tour[]>(url)
+    const result = tourSchema.array().safeParse(response.data);
+        if (!result.success) {
+          console.log(result.error.message);
+          throw new Error('failed to parse data');
+          
+        }
+        return result.data;
+  }
