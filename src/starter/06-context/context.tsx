815a8b9235838e45996f85e607a884453d10763e
Joe Masu <josephmuhindo089@gmail.com>
2024-07-08 10:39:02 +0300

    context API

--- src/starter/06-context/context.tsx
+++ src/starter/06-context/context.tsx
@@ -1,12 +1,21 @@
 import React, { createContext, useContext, useState } from "react";
 
+type Theme = 'light' | 'dark' |'system';
+type ThemeProviderState = {
+    theme: Theme;
+    setTheme: (theme:Theme)=>void;
+}
+const ThemeProviderContext = createContext<ThemeProviderState | undefined>(undefined);
+type ThemeProviderProps = {
+    children: React.ReactNode;
+    defaultTheme?:Theme;
+}
 
-const ThemeProviderContext = createContext<{name: string} | undefined>(undefined);
+export function ThemeProvider({children, defaultTheme = 'system'}:ThemeProviderProps){
+    const [theme, setTheme] = useState<Theme>(defaultTheme);
 
-
-export function ThemeProvider({children}:{children:React.ReactNode}){
     return(
-        <ThemeProviderContext.Provider value={{name: 'hello'}}>
+        <ThemeProviderContext.Provider value={{theme, setTheme}}>
             {children}
         </ThemeProviderContext.Provider>
     );
